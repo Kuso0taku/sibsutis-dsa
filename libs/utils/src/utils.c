392e86558b5f66815186da2ffa9d9f0cebcf69c1
Kuso0taku <denustuzan@gmail.com>
2026-02-17 02:49:29 +0700

    chng accuracy time func

--- libs/utils/src/utils.c
+++ libs/utils/src/utils.c
@@ -1,12 +1,18 @@
-#include <sys/time.h>
+//#include <sys/time.h>
+#include <time.h>
 #include <stdlib.h>
 #include <utils.h>
 
 // current time
 double wtime() {
+  /*
   struct timeval t;
   gettimeofday(&t, NULL);
-  return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+  return (double)t.tv_sec + (double)t.tv_usec * 1E-9;
+  */
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return ts.tv_sec + ts.tv_nsec * 1E-9;
 }
 
 // generate random number
