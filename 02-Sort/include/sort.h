20366849ce923801beb5e5f6725d95f9142a5ea1
Kuso0taku <denustuzan@gmail.com>
2026-02-22 19:28:59 +0700

    02 sort.h

---
+++ 02-Sort/include/sort.h
@@ -0,0 +1,8 @@
+#include <stddef.h> // size_t
+#include <inttypes.h> // uint32_t
+
+void radix_sort(uint32_t*, size_t);
+void merge(uint32_t*, size_t, uint32_t);
+void merge_sort(uint32_t*, size_t);
+void heap_sort(uint32_t*, size_t);
+void selection_sort(uint32_t*, size_t);
